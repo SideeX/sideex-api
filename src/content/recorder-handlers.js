d192b750afcb39a7fdfa7f020a7fbbfeb38dc58c
qwe661234 <qwe661234@gmail.com>
2019-05-13 11:35:55 +0800

    feature: mouseOver

--- src/content/recorder-handlers.js
+++ src/content/recorder-handlers.js
@@ -50,5 +50,77 @@ export function recorderHandlersInit() {
         } while (element);
         this.record("doubleClickAt", this.locatorBuilders.buildAll(event.target), left + ',' + top);
     }, true);
+    Recorder.addEventHandlerVar("nowNode", 0);
+    Recorder.addEventHandlerVar("pageLoaded", true);
+    Recorder.addEventHandler('mouseOver', 'mouseover', function(event) {
+        if (window.document.documentElement)
+            {this.nowNode = window.document.documentElement.getElementsByTagName('*').length;
+            console.log("pass");
+        }
+        var self = this;
+        if (this.pageLoaded === true) {
+            var clickable = this.findClickableElement(event.target);
+        if (clickable) {
+            this.nodeInsertedLocator = event.target;
+                this.nodeInsertedLocator = event.target;
+                setTimeout(function() {
+                    delete self.nodeInsertedLocator;
+                }.bind(self), 500);
+            
+                this.nodeAttrChange = this.locatorBuilders.buildAll(event.target);
+                this.nodeAttrChangeTimeout = setTimeout(function() {
+                    delete self.nodeAttrChange;
+                }.bind(self), 10);
+            }
+            //drop target overlapping
+            if (this.mouseoverQ) //mouse keep down
+            {
+                if (this.mouseoverQ.length >= 3)
+                    this.mouseoverQ.shift();
+                this.mouseoverQ.push(event);
+            }
+        }
+    }, true);
+    Recorder.addEventHandler('mouseOver', 'DOMNodeInserted', function(event) {
+        if (this.pageLoaded === true && window.document.documentElement.getElementsByTagName('*').length > this.nowNode) {
+            var self = this;
+            if (this.scrollDetector) {
+                //TODO: fix target
+                this.record("runScript", [
+                    [
+                        ["window.scrollTo(0," + window.scrollY + ")", ]
+                    ]
+                ], '');
+                this.pageLoaded = false;
+                setTimeout(function() {
+                    this.pageLoaded = true;
+                }.bind(self), 550);
+                delete this.scrollDetector;
+                delete this.nodeInsertedLocator;
+            }
+            if (this.nodeInsertedLocator) {
+                this.record("mouseOver", this.locatorBuilders.buildAll(this.nodeInsertedLocator), '');
+                this.mouseoutLocator = this.nodeInsertedLocator;
+                delete this.nodeInsertedLocator;
+                delete this.mouseoverLocator;
+            }
+        }
+    }, true);
+    Recorder.prototype.findClickableElement = function(e) {
+        if (!e.tagName) return null;
+        var tagName = e.tagName.toLowerCase();
+        var type = e.type;
+        if (e.hasAttribute("onclick") || e.hasAttribute("href") || tagName == "button" ||
+            (tagName == "input" &&
+                (type == "submit" || type == "button" || type == "image" || type == "radio" || type == "checkbox" || type == "reset"))) {
+            return e;
+        } else {
+            if (e.parentNode != null) {
+                return this.findClickableElement(e.parentNode);
+            } else {
+                return null;
+            }
+        }
+    };
 }
 
