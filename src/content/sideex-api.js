371b66b1d1de9d74fd0f1b9c95777cf9420cefb7
qwe661234 <qwe661234@gmail.com>
2019-05-20 12:56:13 +0800

    play Drag And Drop

--- src/content/sideex-api.js
+++ src/content/sideex-api.js
@@ -316,6 +316,77 @@ Sideex.commands = {
         var clientXY = this.getClientXY(element, coordString);
         this.browserBot.fireMouseEvent(element, 'mouseover', true, clientXY[0], clientXY[1]);
     },
+    async dragAndDrop(locator, movementsString, coordString) {
+        /** Drags an element a certain distance and then drops it
+         * @param locator an element locator
+         * @param movementsString offset in pixels from the current location to which the element should be moved, e.g., "+70,-300"
+         */
+        var element = this.browserBot.findElement(locator, coordString);
+        var clientStartXY = getClientXY(element)
+        var clientStartX = clientStartXY[0];
+        var clientStartY = clientStartXY[1];
+    
+        var movements = movementsString.split(/,/);
+        var movementX = Number(movements[0]);
+        var movementY = Number(movements[1]);
+    
+        var clientFinishX = ((clientStartX + movementX) < 0) ? 0 : (clientStartX + movementX);
+        var clientFinishY = ((clientStartY + movementY) < 0) ? 0 : (clientStartY + movementY);
+    
+        var mouseSpeed = this.mouseSpeed;
+        var move = function(current, dest) {
+            if (current == dest) return current;
+            if (Math.abs(current - dest) < mouseSpeed) return dest;
+            return (current < dest) ? current + mouseSpeed : current - mouseSpeed;
+        }
+    
+        this.browserBot.triggerMouseEvent(element, 'mousedown', true, clientStartX, clientStartY);
+        this.browserBot.triggerMouseEvent(element, 'mousemove', true, clientStartX, clientStartY);
+        var clientX = clientStartX;
+        var clientY = clientStartY;
+    
+        while ((clientX != clientFinishX) || (clientY != clientFinishY)) {
+            clientX = move(clientX, clientFinishX);
+            clientY = move(clientY, clientFinishY);
+            this.browserbot.triggerMouseEvent(element, 'mousemove', true, clientX, clientY);
+        }
+    
+        this.browserBot.triggerMouseEvent(element, 'mousemove', true, clientFinishX, clientFinishY);
+        this.browserBot.triggerMouseEvent(element, 'mouseup', true, clientFinishX, clientFinishY);
+    },
+    
+    async dragAndDropToObject(locatorOfObjectToBeDragged, locatorOfDragDestinationObject) {
+        /** Drags an element and drops it on another element
+         *
+         * @param locatorOfObjectToBeDragged an element to be dragged
+         * @param locatorOfDragDestinationObject an element whose location (i.e., whose center-most pixel) will be the point where locatorOfObjectToBeDragged  is dropped
+         */
+        if (!this.browserBot.findElement(locatorOfObjectToBeDragged).draggable) {
+            //origin code
+            var startX = this.getElementPositionLeft(locatorOfObjectToBeDragged);
+            var startY = this.getElementPositionTop(locatorOfObjectToBeDragged);
+    
+            var destinationLeftX = this.getElementPositionLeft(locatorOfDragDestinationObject);
+            var destinationTopY = this.getElementPositionTop(locatorOfDragDestinationObject);
+            var destinationWidth = this.getElementWidth(locatorOfDragDestinationObject);
+            var destinationHeight = this.getElementHeight(locatorOfDragDestinationObject);
+    
+            var endX = Math.round(destinationLeftX + (destinationWidth / 2));
+            var endY = Math.round(destinationTopY + (destinationHeight / 2));
+    
+            var deltaX = endX - startX;
+            var deltaY = endY - startY;
+    
+            var movementsString = "" + deltaX + "," + deltaY;
+            this.doDragAndDrop(locatorOfObjectToBeDragged, movementsString);
+        } else {
+            // © Shuo-Heng Shih, SideeX Team
+            var element = this.browserBot.findElement(locatorOfObjectToBeDragged);
+            var target = this.browserBot.findElement(locatorOfDragDestinationObject);
+            this.browserBot.triggerDragEvent(element, target);
+            // END
+        }
+    },
     /**
      * Opens an URL in the test frame. This accepts both relative and absolute
      * URLs.
