dcc339ccf79e47463e8ee26391afd9a0204d16b2
Ming Hsiu Tsai <jamesscamel@gmail.com>
2019-07-10 11:01:39 +0800

    Feature: add api

---
+++ src/panel/js/background/api.js
@@ -0,0 +1,72 @@
+import { browser } from "webextension-polyfill-ts";
+
+export default {
+    toolBar: {
+        clickPlayButton: function (mode) {
+            console.log(mode);
+            Panel.recorder.isRecord = false;
+            Panel.playback.isPlay = true;
+            Panel.recorder.detach();
+            switch (mode) {
+                case "Play this case": {
+                    Panel.playback.doPlay(0, 0); // Playback.PLAY_CASE
+                    break;
+                }
+                case "Play this suite": {
+                    Panel.playback.doPlay(1, 0); // Playback.PLAY_SUITE
+                    break;
+                }
+                case "Play all suites": {
+                    Panel.playback.doPlay(2, 0); // Playback.PLAY_ALL_SUITES
+                    break;
+                }
+            }
+            EntryPoint.toolBar.syncButtonState();
+        },
+        clickRecordButton: function () {
+            Panel.recorder.isRecord = !Panel.recorder.isRecord;
+            if (Panel.recorder.isRecord) {
+                console.log("Recording");
+                Panel.recorder.attach();
+                Panel.recorder.notificationCount = 0;
+                Panel.recorder.prepareRecord();
+
+                Panel.recorder.isRecord = true;
+            } else {
+                console.log("Stop");
+                Panel.recorder.detach();
+                Panel.recorder.isRecord = false;
+            }
+            EntryPoint.toolBar.syncButtonState();
+            EntryPoint.fileList.syncFiles();
+        },
+        clickStopButton: function () {
+            Panel.playback.stop();
+
+            Panel.playback.isPlay = false;
+            EntryPoint.toolBar.syncButtonState();
+        },
+        clickPauseButton: function () {
+            Panel.playback.pause();
+
+            Panel.playback.isPlay = false;
+            Panel.playback.isPause = true;
+            EntryPoint.toolBar.syncButtonState();
+        },
+        clickResumeButton: function () {
+            Panel.playback.resume();
+
+            Panel.playback.isPlay = true;
+            Panel.playback.isPause = false;
+            EntryPoint.toolBar.syncButtonState();
+        },
+        clickSettingButton: function () {
+            browser.runtime.openOptionsPage();
+        },
+        changeSpeed: function (value) {
+            Panel.setting.set({ delay: 500 * (5 - value) });
+            EntryPoint.toolBar.updateSpeed(parseInt(value));
+        }
+
+    }
+};
