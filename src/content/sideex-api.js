095b8dda983d5f3867ec2e4311d38dbd2cb49ee5
qwe661234 <qwe661234@gmail.com>
2020-05-15 15:01:31 +0800

    Bug: revise copyright

--- src/content/sideex-api.js
+++ src/content/sideex-api.js
@@ -1,6 +1,6 @@
 /* eslint-disable no-redeclare */
 /*
- * Copyright 2011 Software Freedom Conservancy
+ * Copyright Software Freedom Conservancy
  *
  *  Licensed under the Apache License, Version 2.0 (the "License");
  *  you may not use this file except in compliance with the License.
